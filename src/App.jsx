2e86bde4ce8222ac169d4ebed4203217659549d3
DEvbrat singh <devbratsingh341@gmail.com>
2023-10-16 09:23:47 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'; 
+
+import {fetchDataFromApi} from "./utils/api"
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration, getGenres } from './store/homeSlice'
+
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import Explore from './pages/explore/Explore';
+import SearchResult from './pages/searchResult/SearchResult';
+import PageNotFound from "./pages/404/pageNotFound"
+import Footer from './components/footer/Footer'
+
+
+function App() {
+
+  const dispatch = useDispatch()
+  const {url} = useSelector((state) => state.home);
+  
+  
+  useEffect(() =>{
+    // apiTesting();
+    fetchApiConfig()
+    genresCall()
+  }, [])
+
+  // const apiTesting = () =>{
+  //   fetchDataFromApi("/movie/popular")
+  //   .then((res) => {
+  //     console.log(res)
+  //     dispatch(getApiConfiguration(res))
+  //   })   
+  // }
+
+  const fetchApiConfig = () =>{
+    fetchDataFromApi("/configuration")
+    .then((res) => {
+      console.log(res)
+
+      const url = {
+        backdrop : res.images.secure_base_url  + "original",
+        poster : res.images.secure_base_url  + "original",
+        profile : res.images.secure_base_url  + "original",
+      }
+
+      dispatch(getApiConfiguration(url))
+    })   
+  }
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+        promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    
+    data.map(({ genres }) => {
+        return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
+
+  return (  
+    <Router>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </Router> 
+  ) 
+}
+
+export default App
